bf9325eb6419aca4233fe7593adfbb391942fd99
zengliq <zengliq@homeking365.com>
2025-09-08 14:45:54 +0800

    feat: vant nutui 配置

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,12 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { Add } from '@nutui/icons-vue'
+</script>
 
 <template>
+  <Add color="red" />
+  <nut-button type="primary">nutui</nut-button>
+  <van-icon name="chat-o" />
+  <van-button type="primary">按钮</van-button>
   <h1>You did it!</h1>
   <p>
     Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,11 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import AutoImport from 'unplugin-auto-import/vite';
+import Components from 'unplugin-vue-components/vite';
+import { VantResolver } from '@vant/auto-import-resolver';
+import NutUIResolver from '@nutui/auto-import-resolver'
+
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -11,6 +16,12 @@ export default defineConfig({
     vue(),
     vueJsx(),
     vueDevTools(),
+    AutoImport({
+      resolvers: [VantResolver()],
+    }),
+    Components({
+      resolvers: [VantResolver(), NutUIResolver()],
+    }),
   ],
   resolve: {
     alias: {
